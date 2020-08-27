9e90631986e810732e110c0fc3bb46bca658cc7d
Abdullah <21100270@lums.edu.pk>
2020-08-27 14:48:13 +0500

    added image to header

--- Navigation/appnavigator.js
+++ Navigation/appnavigator.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { View, Button } from 'react-native';
-import Home from '../screens/Home';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { View, Button, ImageBackground } from "react-native";
+import Home from "../screens/Home";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Stack = createStackNavigator();
 
@@ -14,38 +14,41 @@ const Stack = createStackNavigator();
 //     );
 
 // }
-const Settings = ({navigation}) => {
-    return(
-        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-            <Button title="Settings" onPress={()=>navigation.navigate("Home")}/>
-        </View>
-    );
-    
-}
+const Settings = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="Settings" onPress={() => navigation.navigate("Home")} />
+    </View>
+  );
+};
 
-const CustomHeader = ({navigation})=>(
-  <View 
+const CustomHeader = ({ navigation }) => (
+  <ImageBackground
     style={{
-      height:55,
-      backgroundColor:"red",
-      flexDirection:"row",
-      elevation:4,
-      justifyContent:"space-between",
-      alignItems:"center",
-      padding:15
-    }} 
+      height: 55,
+      flexDirection: "row",
+      elevation: 4,
+      justifyContent: "space-between",
+      alignItems: "center",
+      padding: 15,
+    }}
+    source={require("../assets/header.jpg")}
   >
-    <Icon name="menu" color="white" size={30} onPress={()=>navigation.openDrawer()} />
-  </View>
-)
-
+    <Icon
+      name="menu"
+      color="white"
+      size={30}
+      onPress={() => navigation.openDrawer()}
+    />
+  </ImageBackground>
+);
 
 function MyStack() {
   return (
-    <Stack.Navigator 
+    <Stack.Navigator
       screenOptions={{
-        header:(props)=><CustomHeader {...props} />
-      }} 
+        header: (props) => <CustomHeader {...props} />,
+      }}
     >
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Settings" component={Settings} />
