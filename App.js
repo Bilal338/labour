f27bf45ea73990acf9038a6cc4b32cf47263b43e
Abdullah <21100270@lums.edu.pk>
2020-09-10 21:21:58 +0500

    merging and resolving conflicts

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
-import React from 'react';
+import React from "react";
 // import { View, Text, StyleSheet } from 'react-native';
 // import Logopage from './src/Screens/logopage.js';
 // import PNumber from './src/Screens/pNumber.js';
-import Verify from './src/Screens/Verify.js';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import MyDrawer from './Navigation/drawerNavigator';
+import Verify from "./src/Screens/Verify.js";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import MyDrawer from "./Navigation/drawerNavigator";
 //comment
 const App = () => {
   return (
-<<<<<<< HEAD
-  //  <NavigationContainer>
-  //    <MyStack/>
-  //   </NavigationContainer>
-  <Verify/>
-=======
-   <NavigationContainer>
-     <MyDrawer/>
+    <NavigationContainer>
+      <MyDrawer />
     </NavigationContainer>
->>>>>>> 9e90631986e810732e110c0fc3bb46bca658cc7d
   );
 };
 
